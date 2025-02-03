ca8e1cfa2dc218695daef6f4f115eef508400c60
Ben10 <Ben10@gmail.com>
2025-02-04 00:39:08 +0530

    Initial

---
+++ Titanic-Survival.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+train = pd.read_csv('D:\\Sem6\\ML-Projects\\train.csv')
+test = pd.read_csv('D:\\Sem6\\ML-Projects\\train.csv')
+
+print(train.head(5))
+
+print(train.info())
+
+print(train.shape)
+
+print(train.isnull().sum())
+
+train = train.drop(columns='Cabin',axis=1)
+train.fillna({'Age': train['Age'].mean()}, inplace=True)
+train.fillna({'Embarked':train['Embarked'].mode()[0]},inplace=True)
+
+print(train.isnull().sum())
+
+print(train['Embarked'].mode())
+print(train['Embarked'].mode()[0])
+
+print(train['Survived'].value_counts())
+
+sns.set()
+sns.countplot(x="Survived",data=train)
+sns.countplot(x="Embarked",data=train)
+plt.show()
